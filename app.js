33071dc3f8b8aaa3ec3c3ecc71e6e0bcf98d4754
maazjnr <91166282+maazjnr@users.noreply.github.com>
2022-05-19 02:35:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+const countdown = () => {
+const countDate = new Date('May 21, 2023 00:00:00').getTime();
+   const now = new Date().getTime();
+   const gap = countDate - now;
+
+   //How the fuck does time work?
+   const second = 1000;
+   const minute = second * 60;
+   const hour = minute * 60;
+   const day = hour * 24;
+
+   //Calculate the shit
+   const textDay = Math.floor(gap / day);
+   const textHour = Math.floor((gap % day) / hour);
+   const textMinute = Math.floor((gap % hour) / minute);
+   const textSecond = Math.floor((gap % minute) / second);
+
+   document.querySelector(".day").innerText = textDay;
+   document.querySelector(".hour").innerText = textHour;
+   document.querySelector(".minute").innerText = textMinute;
+   document.querySelector(".second").innerText = textSecond;
+
+};
+
+
+
+setInterval(countdown, 1000)
